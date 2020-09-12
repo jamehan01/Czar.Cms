d2880e78a1514ac1e890bed4a42396d8416b6d8d
jamehan95 <hn15550441575@163.com>
2020-09-12 18:14:07 +0800

    Dapper简单练习

---
+++ Sample05/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample05
+{
+   public class Comment
+    {
+        public string id { get; set; }//主键
+        public int content_id { get; set; }//文章ID
+        public string content { get; set; }
+        public DateTime add_time { get; set; } = DateTime.Now;
+    }
+}
---
+++ Sample05/Program.cs
@@ -0,0 +1,60 @@
+﻿using Dapper;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+namespace Sample05
+{
+    class Program
+    {
+        ///<summary>
+        ///测试插入单条数据
+        /// </summary>
+        static void test_insert()
+        {
+            var content = new Content
+            {
+                title = "标题1",
+                content = "内容1",
+            };
+            using (var conn = new SqlConnection("Data Source=127.0.0.1;User ID=sa;Password=hn123654!;Initial Catalog=CMS;Pooling=true;Max Pool Size=100;"))
+            {
+                string sql_insert = @"INSERT INTO [Content]
+                    (title,[content],status,add_time,modify_time)
+                    VALUES (@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql_insert, content);
+                Console.WriteLine($"test_insert:插入了{result}条数据!");
+            }
+        }
+        static void test_mult_insert()
+        {
+            List<Content> contents = new List<Content>()
+            {
+                new Content
+                {
+                    title = "批量插入标题1",
+                    content = "批量插入内容1",
+                },
+                 new Content
+                {
+                    title = "批量插入标题2",
+                    content = "批量插入内容2",
+                },
+
+            };          
+            using (var conn = new SqlConnection("Data Source=127.0.0.1;User ID=sa;Password=hn123654!;Initial Catalog=CMS;Pooling=true;Max Pool Size=100;"))
+            {
+                string sql_insert = @"INSERT INTO [Content]
+                    (title,[content],status,add_time,modify_time)
+                    VALUES (@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql_insert, contents);
+                Console.WriteLine($"test_insert:插入了{result}条数据!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            /*test_insert();*/
+            test_mult_insert();
+        }
+    }
+}
